8c381e382184dd68462c09d8909c9fe35a2ddbe6
Julieta Mullen <mul.julieta@gmail.com>
2021-04-22 00:10:02 -0300

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div className="about" id="about">
             <h2>About me</h2>
-            <p>Hi there! &#x1f44b; I'm a self taught softaware development student based in Argentina.</p>
+            <p>Hi there! &#x1f44b; I'm a self taught software development student based in Argentina.</p>
             <p>Former bartender, a while ago I started learning about JavaScript, HTML and CSS. I found this development world incredibly interesting, and kept the learning going.</p>
             <p>Currently, I'm getting into ReactJS and would like to learn React Native in the future. I would also love to handle new technologies and improve my practices to make my projects more accesible to everyone!</p>
             <p>I've got knowledge about <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b> and <b>ReactJS</b>, but I'm eager to use other tools as well!</p>
